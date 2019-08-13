c16ccfa7f8ab00ab843ebcbc5740b12e68638b78
Evin Sellin <evinism@gmail.com>
2019-08-12 18:55:50 -0700

    initial commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', (req, res) => res.status(200).send({
+  title: 'Polygive.',
+}));
+
+module.exports = app;
