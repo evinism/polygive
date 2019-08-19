5de8e7c8f82919c19c97b7783dd89cbaaf8991ec
Evin Sellin <evinism@gmail.com>
2019-08-18 22:40:33 -0700

    move to shared state hook

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,28 +7,30 @@ const apiUrl = process.env.REACT_APP_BACKEND_URL;
 
 function App() {
   // Impossible states impossible mega-violation below:
-  const [loading, setLoading] = useState(true);
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [user, setUser] = useState();
+  const [state, setState] = useState({
+    loading: true,
+    loggedIn: false,
+    user: undefined,
+  });
 
   useEffect(() => {
     fetch(apiUrl + '/user/current', { credentials: 'include' })
       .then(response => response.json())
       .then(data => {
-        setLoading(false);
-        if (data.loggedIn) {
-          setUser({
-            name: data.name,
-          });
-        }
-        setLoggedIn(data.loggedIn);
+        setState({
+          loading: false,
+          loggedIn: data.loggedIn,
+          user: data.loggedIn
+            ? { name: data.name }
+            : undefined,
+        });
       });
   }, []);
 
   return (
     <div className="App">
-      {!loading && 
-        (loggedIn ? <Home user={user}/> : <LoggedOut />)}
+      {!state.loading && 
+        (state.loggedIn ? <Home user={state.user}/> : <LoggedOut />)}
     </div>
   );
 }
