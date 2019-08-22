65220d4a7c0daec913cc99cfd7a810df072214c4
Evin Sellin <evinism@gmail.com>
2019-08-21 17:59:58 -0700

    fix cors posts

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -52,7 +52,7 @@ module.exports = (app) => {
     cors(corsConfig),
     ensureSuper(controllers.donation.all));
 
-  app.options("/*", function(req, res, next){
+  app.options("/*", cors(corsConfig), function(req, res, next){
     res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     //res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
