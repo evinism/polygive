2b0ba1b6ba09f8b707bc730414611a056581419c
Evin Sellin <evinism@gmail.com>
2020-01-05 19:22:19 -0800

    add a basic paginator component

--- client/src/components/Paginator/index.tsx
+++ client/src/components/Paginator/index.tsx
@@ -6,23 +6,43 @@ interface PaginatorProps {
   setPage: (page: number) => void;
 }
 
+function onlyUnique(value: number, index: number, self: number[]) {
+  return self.indexOf(value) === index;
+}
+
 const Paginator = ({
   page: currentPage,
   totalPages,
   setPage
-}: PaginatorProps) => (
-  <div>
-    {Array(totalPages)
-      .fill(0)
-      .map((_, i) => {
-        const page = i + 1;
-        return currentPage !== page ? (
-          <button onClick={() => setPage(page)}>{page}</button>
-        ) : (
-          page
-        );
-      })}
-  </div>
-);
+}: PaginatorProps) => {
+  const pagesToDisplay = [
+    1,
+    currentPage - 1,
+    currentPage,
+    currentPage + 1,
+    totalPages
+  ]
+    .filter(page => page > 0 && page <= totalPages)
+    .filter(onlyUnique);
+  pagesToDisplay.sort((a, b) => a - b);
+  const renderArray: JSX.Element[] = [];
+  for (let i = 0; i < pagesToDisplay.length; i++) {
+    const page = pagesToDisplay[i];
+    renderArray.push(
+      currentPage !== page ? (
+        <button onClick={() => setPage(page)}>{page}</button>
+      ) : (
+        <span>{page}</span>
+      )
+    );
+
+    const followingLink = pagesToDisplay[i + 1];
+    if (followingLink && followingLink > page + 1) {
+      renderArray.push(<span>...</span>);
+    }
+  }
+
+  return <div>{renderArray}</div>;
+};
 
 export default Paginator;
